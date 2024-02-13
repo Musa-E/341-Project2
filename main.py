9ae65fd7ded7da5312210192c5c6e513577b7042
Musa Elqaq <melqa2@uic.edu>
2024-02-13 00:12:30 -0600

    Formatted header comment in main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #
-# header comment!
+# Author: Musa Elqaq
+# Class: CS 341, Spring 2024
 #
+# Program 2 - Chicago Lobbyist Database App
+#
+# The goal of this project is to write a console-based database 
+# application in Python, this time using an N-tier design. The 
+# database used for this project consists of information pertaining 
+# to registered lobbyists in Chicago, their employers, their clients, 
+# and their compensation.
 import sqlite3
 import objecttier
 
